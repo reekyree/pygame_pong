ec35eefc8d2622e23c3d0cb94444a9de67f0732d
reekyree <bl.luster.32@gmail.com>
2023-09-02 23:15:43 -0500

    Created a white rectangle and drew it to the screen

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ screen = pygame.display.set_mode((500, 400))
 # Set the title of the window
 pygame.display.set_caption("Pygame Pong")
 
+# Draw a rectangle to the screen
+pygame.draw.rect(screen, "white", (50, 50, 100, 100))
+
+# Update the screen
+pygame.display.flip()
+
 # Set variable for running the game
 running = True
 
